be5531d96943fea32a0c73b81452a7957c9c4881
Niclnt <nico_agus_27@hotmail.com>
2025-04-04 20:02:29 -0300

    Primer version, falta test de inversas

--- src/Library.hs
+++ src/Library.hs
@@ -4,40 +4,42 @@ import PdePreludat
 -- 1. Numeros
 
 siguiente :: Number -> Number
-siguiente = implementame
+siguiente num = num + 1
 
 esPositivo :: Number -> Bool
-esPositivo = implementame
+esPositivo num = num > 0
 
--- escriban el tipo de esta función
-inversa n = implementame
+inversa :: Number -> Number
+inversa num = 1/num
 
 -- 2. Temperaturas
 
-celsiusAFahrenheit :: Number -> Number
-celsiusAFahrenheit celsius = implementame
+celsiusAFahrenheit :: Number->Number
+celsiusAFahrenheit tempC = (tempC * 1.8) + 32
 
-fahrenheitACelsius :: Number -> Number
-fahrenheitACelsius fahrenheit = implementame
-
--- escriban el tipo de esta función
-haceFrioCelsius grados = implementame
+fahrenheitACelsius :: Number->Number
+fahrenheitACelsius tempF = (tempF-32)/1.8
 
 -- escriban el tipo de esta función
-haceFrioFahrenheit grados = implementame
+haceFrioCelsius :: Number -> Bool
+haceFrioCelsius tempC = tempC <= 8
+
+-- escriban el tipo de esta función
+haceFrioFahrenheit :: Number -> Bool
+haceFrioFahrenheit tempF = fahrenheitACelsius tempF <= 8
 
 -- 2.5 Bonus OPCIONAL
 perimetroCirculo :: Number -> Number
-perimetroCirculo radio = implementame
+perimetroCirculo radio = 2 * pi * radio
 
 perimetroCuadrado :: Number -> Number
-perimetroCuadrado lado = implementame
+perimetroCuadrado lado = lado * 4
 
-superficieCuadrado :: Number -> Number
-superficieCuadrado lado = implementame
+superficieCuadrado :: Number->Number
+superficieCuadrado lado = lado * lado
 
-superficieCubo :: Number -> Number
-superficieCubo lado = implementame
+superficieCubo :: Number->Number
+superficieCubo lado = 6 * lado * lado
 
-superficieCilindro :: Number -> Number -> Number
-superficieCilindro radio altura = implementame
+superficieCilindro :: Number -> Number ->Number
+superficieCilindro radio altura = 2*pi*radio*(radio+altura)
